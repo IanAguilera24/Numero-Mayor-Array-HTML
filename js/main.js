a6bf268cd1cf67289361c44f2d3c4782e6a69ca6
IanAguilera24 <aguilera.marquez.ianalexis@gmail.com>
2023-03-02 23:33:54 -0600

    1ra version del numero mayor de un array

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var numeros = [];
+
+function narray() {
+    var numero = document.getElementById("valores").value;
+    numero = Number(numero); //convierte el valor introducido a un numero
+    if (!isNaN(numero)) {
+        numeros.push(numero);   
+        var arreglo = document.getElementById("arreglo");
+        arreglo.innerHTML = "El arreglo actual es: " + numeros.join(", ");
+    } else {
+        alert("Ingresa un numero valido");
+    }
+    document.getElementById("valores").value = "";
+
+    var maximo = Math.max.apply(null, numeros); //numero mas grande de un arreglo
+    var resultado = document.getElementById("resultado");
+    resultado.innerHTML = "El numero mas grande es: " + maximo;
+}
